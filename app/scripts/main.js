28abb0e65c9f30960db42c2cd9f35bb30d676ec1
Yura Bazaliy <yurchykxxx@gmail.com>
2016-04-01 17:22:07 +0300

    Adding styles and blocks for all project

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,19 @@
 // Search
 $(function () {
+
   $('#search').on('focus', function() {
      if($(this).val() == 'Пошук:') {
        $(this).val('');
      }
   });
+
   $('#search').on('blur', function() {
     if( $(this).val() == '' ) {
       $(this).val('Пошук:');
     }
-  })
+  });
+
+  // Create a new instance of Headhesive.js and pass in some options
+  var header = new Headhesive('.panel');
+  
 });
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -205,7 +205,7 @@ gulp.task('wiredep', () => {
     }))
     .pipe(gulp.dest('app/styles'));
 
-  gulp.src('app/*.html')
+  gulp.src('app/*.jade')
     .pipe(wiredep({
       exclude: ['bootstrap-sass'],
       ignorePath: /^(\.\.\/)*\.\./
