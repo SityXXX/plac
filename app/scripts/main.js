24ec790ad051931e69297565d3cb52410807defe
Bazaliy Yura <yurchykxxx@gmail.com>
2016-04-04 09:53:22 +0300

    Editing footer and menu

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,23 @@
 // Search
-$(function () {
-
-  $('#search').on('focus', function() {
-     if($(this).val() == 'Пошук:') {
-       $(this).val('');
-     }
-  });
-
-  $('#search').on('blur', function() {
-    if( $(this).val() == '' ) {
-      $(this).val('Пошук:');
-    }
-  });
+$(function() {
 
   // Create a new instance of Headhesive.js and pass in some options
   var header = new Headhesive('.panel');
-  
+
+  $('.search__field').each(function() {
+
+    $(this).on('focus', function() {
+      if ($(this).val() == 'Пошук:') {
+        $(this).val('');
+      }
+    });
+
+    $(this).on('blur', function() {
+      if ($(this).val() == '') {
+        $(this).val('Пошук:');
+      }
+    });
+
+  });
+
 });
